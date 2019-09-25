ac5e0b534a63e7fba32974402c9d07334134829e
Wendeley Fernandes <wendeleyti@gmail.com>
2019-09-25 18:24:00 -0300

    Adicionando filtro de pesquisa dos Robôs

--- src/robots.js
+++ src/robots.js
@@ -22,5 +22,29 @@ export const robots = [
     name: "Patricia Lebsack",
     username: "Karianne",
     email: "Julianne.Oconner@kory.org"
+  },
+  {
+    id: 5,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "sincere@april.biz"
+  },
+  {
+    id: 6,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "shanna@melissa.tv"
+  },
+  {
+    id: 7,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net"
+  },
+  {
+    id: 8,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.Oconner@kory.org"
   }
 ];
