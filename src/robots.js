d1a04737c010de868ac8afe66f149da7be97a88f
Wendeley Fernandes <wendeleyti@gmail.com>
2019-09-25 17:11:16 -0300

    Criando cards dos robôs

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "sincere@april.biz"
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "shanna@melissa.tv"
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net"
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.Oconner@kory.org"
+  }
+];
