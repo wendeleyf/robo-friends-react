8c744966b55168e1053c125b5b49113c454eaa82
Wendeley Fernandes <wendeleyti@gmail.com>
2019-10-19 16:33:28 -0300

    Adicionando componente de Scroll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = event => {
@@ -22,13 +28,19 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />,
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="navy">Robo Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />,
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ name, email, id }) {
   return (
-    <div className="bg-light-purple tc dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
         <h2>{name}</h2>
